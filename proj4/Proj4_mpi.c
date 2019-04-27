0a3d08120f37bf9ae26146e3a65311c4776e8446
Rida Ahmed <31077599+rida300@users.noreply.github.com>
2019-04-26 19:13:56 -0500

    I think this works

--- proj4/Proj4_mpi.c
+++ proj4/Proj4_mpi.c
@@ -1,185 +1,311 @@
-#include "mpi.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "mpi.h"
 
+#define MAX_ENTRIES 1000000		// longest file size will be 1000000 entries long
+#define MAX_STRING_SIZE 10000
+#define send_tag 1000
 
-#define ARRAY_SIZE 5
-#define ARTICLE_SIZE 100000
-#define STRING_SIZE 1000
-#define NUM_THREADS 2
+/* holds each line of the file */
+char File_Contents[MAX_ENTRIES][MAX_STRING_SIZE];
 
-char File_Contents[ARRAY_SIZE][ARTICLE_SIZE];
-void *  find_longest_substring(int  id, char ** LCS, int linesPT);
-int init_array(FILE *);
-void print_results(char **);
+void print (char x[], int i, int j, char **b, int *iptr, char *r)
+{
+	if (i == 0 || j == 0)
+  	return;
+	if (b[i][j] == 'c')
+	{
+    print (x, i-1, j-1, b, iptr, r);
+    r[(*iptr)++] = x[i-1];
+	}
+	else if (b[i][j] == 'u')
+  	print (x, i-1, j, b, iptr, r);
+	else
+    print (x, i, j-1, b, iptr, r);
+}
 
-int main() {
+void lcs (char x[], char y[], char **b, int **c)
+{
+	int i, j, m, n;
+	
+	m = strlen (x);
+	n = strlen (y);
+	
+	for (i = 0; i <= m; i++)
+  	c[i][0] = 0;
+	for (i = 0; i <= n; i++)
+    c[0][i] = 0;
+	              
+	//c, u and l denotes cross, upward and downward directions respectively
+	for (i = 1; i <= m; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+		  if (x[i-1] == y[j-1])
+		  {
+		    c[i][j] = c[i-1][j-1] + 1;
+		    b[i][j] = 'c';
+		  }
+		  else if (c[i-1][j] >= c[i][j-1])
+		  {
+		    c[i][j] = c[i-1][j];
+		    b[i][j] = 'u';
+		  }
+		  else
+		  {
+		    c[i][j] = c[i][j-1];
+		    b[i][j] = 'l';
+		  }
+		}
+	}
+}
 
-    int rank, number_of_proc, i, start_index, end_index;
+/* reads file, returns lines read */
+int read_file (const char filename[], const int INPUT_SIZE)
+{
+	FILE *file;
+	char x[MAX_STRING_SIZE];
+	int index;
+	
+	file = fopen (filename, "r");
+	
+	if (file == NULL)
+	{
+		printf ("Bad file\n");
+		return -1;
+	}
+	
+	index = 0;
+	while (fgets (x, MAX_STRING_SIZE, file) != NULL && index < INPUT_SIZE)
+	{
+		strcpy (File_Contents[index], x);
+		index++;
+	}
+	
+	fclose (file);
+	return index; 
+}
+
+int main (int argc, char **argv)
+{
+	// no longer using third arg in program
+	// but need it to run the program anyway
+	if (argc != 4)
+	{
+		printf ("Usage: %s file input_size\n", argv[0]);
+		return -1;
+	}
+	
+	int rank, number_of_proc, i, start_pos, end_pos;
 	double start_time, end_time;
 	
- 
-	// initialize each process
-	MPI_Init ();
+	// Give each process the args
+	MPI_Init (&argc, &argv);
 	// get number of running processes
 	MPI_Comm_size (MPI_COMM_WORLD, &number_of_proc);
 	// get number of this process
 	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
- 
-	FILE * fp;
-  char * LCS[ARRAY_SIZE-1];
-  bool root;
-  MPI_Status status;
-  
-  root = rank == 0;
+	
+	const int INPUT_SIZE = atoi (argv[2]);
+	const int NUM_OF_THREADS = number_of_proc;
+	int lines_read = read_file (argv[1], INPUT_SIZE);
+	
+	if (lines_read < 0)
+		return -1;
+		
+	const int LCS_SIZE = lines_read - 1;
+	const int LINES_PER_THREAD = lines_read % NUM_OF_THREADS == 0 ? lines_read/NUM_OF_THREADS : lines_read/NUM_OF_THREADS + 1;
+	
+	char **LCS;
+	int *result_sizes;
+	bool root;
+	MPI_Status status;
+	
+	root = rank == 0;
 	
 	if (root)
 		start_time = MPI_Wtime ();
-   // if this process is root process
+	
+	// if this process is root process
 	if (root)
 	{
-  
-  fp= fopen ("testLorem.txt", "r");
-	for(int i = 0; i<ARRAY_SIZE-1; i++)
-		LCS[i] = (char *)malloc(sizeof(char) * (STRING_SIZE));
-	
-    
-	int i = init_array(fp);
-	const int LINES_PER_THREAD = i % NUM_THREADS == 0 ? i / NUM_THREADS : i / NUM_THREADS + 1;
-  
-  find_longest_substring(omp_get_thread_num(), LCS, LINES_PER_THREAD);
-
-  // collect results from child processes
+		// allocate memory for LCS buffer
+		LCS = (char **)malloc (LCS_SIZE * sizeof (char *));
+		for (i = 0; i < LCS_SIZE; i++)
+			LCS[i] = (char *)malloc (MAX_STRING_SIZE * sizeof (char));
+		// allocate memory for LCS sizes
+		result_sizes = (int *)malloc (LCS_SIZE * sizeof (int));
+		
+		start_pos = rank * LINES_PER_THREAD;
+		end_pos = start_pos + LINES_PER_THREAD;
+		
+		// get the lcs with parent process
+		int o = start_pos;
+		while (o < end_pos && o < lines_read-1)
+		{
+			int j, m, n, k, index;
+			int xlen = strlen (File_Contents[o]);
+			int ylen = strlen (File_Contents[o+1]);
+			char *x = (char *)malloc ((xlen+1) * sizeof (char)); 			// first string
+			char *y = (char *)malloc ((ylen+1) * sizeof (char));			// second string
+		
+			char **b = (char **)malloc ((xlen+1) * sizeof (char *));
+			int **c = (int **)malloc ((xlen+1) * sizeof (int *));
+		
+			// initialize b and c
+			for (j = 0; j <= xlen; j++)
+			{
+				b[j] = (char *)malloc ((ylen+1) * sizeof (char));
+				c[j] = (int *)malloc ((ylen+1) * sizeof (int));
+			}
+		
+			strncpy (x, File_Contents[o], xlen+1);
+			strncpy (y, File_Contents[o+1], ylen+1);
+		
+			lcs (x, y, b, c);
+		
+			// get lcs lengths
+			m = strlen (x);
+			n = strlen (y);		
+		
+			index = 0;
+			print (x, m, n, b, &index, LCS[o]);
+		
+			// save the size of the LCS string
+			result_sizes[o] = index;
+		
+			for (k = 0; k < xlen; k++)
+			{
+				free (b[k]);
+				free (c[k]);
+			}
+			
+			free (b);
+			free (c);
+			free (x);
+			free (y);
+			o++;
+		}
+		
+		// collect LCS from child processes
 		for (i = 1; i < number_of_proc; i++)
 		{
-			int num_of_results, ye_ol_index, worker_num, p, j, k;
-			int results_array_sizes[LINES_PER_THREAD];
+			int num_of_LCS, ye_ol_index, worker_num, p, j, k;
+			int LCS_array_sizes[LINES_PER_THREAD];
 			char *worker_msg = (char *)malloc (LINES_PER_THREAD * MAX_STRING_SIZE * sizeof (char));
 			
-			// receive number of results being sent by worker
-			MPI_Recv (&num_of_results, 1, MPI_INT, i, send_tag*i+i, MPI_COMM_WORLD, &status);
-			// receive worker's results_array_sizes array
-			MPI_Recv (results_array_sizes, LINES_PER_THREAD, MPI_INT, i, send_tag*i+i+1, MPI_COMM_WORLD, &status);
-			// receive worker's results array
+			// receive number of LCS being sent by worker
+			MPI_Recv (&num_of_LCS, 1, MPI_INT, i, send_tag*i+i, MPI_COMM_WORLD, &status);
+			// receive worker's LCS_array_sizes array
+			MPI_Recv (LCS_array_sizes, LINES_PER_THREAD, MPI_INT, i, send_tag*i+i+1, MPI_COMM_WORLD, &status);
+			// receive worker's LCS array
 			MPI_Recv (worker_msg, LINES_PER_THREAD * MAX_STRING_SIZE, MPI_CHAR, i, send_tag*i+i+2, MPI_COMM_WORLD, &status);
 			
 			ye_ol_index = i * LINES_PER_THREAD;
 			
 			// add result sizes to array
-			for (k = 0; k < num_of_results; k++)
-				result_sizes[ye_ol_index+k] = results_array_sizes[k];
+			for (k = 0; k < num_of_LCS; k++)
+				result_sizes[ye_ol_index+k] = LCS_array_sizes[k];
 			
-			// add worker msg to results array
-			for (p = 0; p < num_of_results; p++)
+			// add worker msg to LCS array
+			for (p = 0; p < num_of_LCS; p++)
 			{
-				for (j = 0; j < results_array_sizes[p]-1; j++)
-					results[ye_ol_index+p][j] = worker_msg[(p*MAX_STRING_SIZE)+j];
+				for (j = 0; j < LCS_array_sizes[p]-1; j++)
+					LCS[ye_ol_index+p][j] = worker_msg[(p*MAX_STRING_SIZE)+j];
 			}
 			
 			free (worker_msg);
 		}
-
-	print_results(LCS);
-	printf("Main: program completed. Exiting.\n");
-}
-
-
-else
-{
-
-}
-
-}
-
-
-
-
-
-int init_array(FILE * fp)
-{
-	int i = 0;
-	if(fp != NULL)
-	{
-		char line [ARTICLE_SIZE]; 
-    		while ( fgets ( line, sizeof line, fp ) != NULL && i < ARRAY_SIZE) 
-      		{
-			strncpy(File_Contents[i++], line, strlen(line));
-
-      		}
-      		fclose ( fp );
-	}
-	return i;
-
-}
-
-void print_results(char ** LCS)
-{
-	int j;	// then print out the totals
-	for ( int i = 0; i < ARRAY_SIZE-1; i++ ) 
-	{
-		j = i+1;
-     		printf(" %d & %d - %s\n",i,j, LCS[i]);
-  	}
-}
-
-void *  find_longest_substring(int  id, char ** LCS, int linesPT)//id is 0,1,2,3
-{
-  
- 
-
- 
-	int startPos, endPos;
-	char local_LCS[ARRAY_SIZE/NUM_THREADS][STRING_SIZE];
-	char substring[STRING_SIZE];
-	int i,j,x,y,maxlen,len, currPos = 0;
-	int length1 = 0;
-	int length2=0;
-	int comp = 0;
-	
-	{
-		startPos = (id) * linesPT;
-		endPos = startPos + linesPT;
-		for(currPos = startPos; currPos < endPos; currPos++)
+		
+		int s;
+		printf ("LCS:\n");
+		for (i = 0; i < LCS_SIZE; i++)
 		{
-			maxlen = 0;
-			length1 = strlen(File_Contents[currPos]);
-			length2 = strlen(File_Contents[currPos+1]);
-			for(i=0; i<=length1; i++)
-			{
-				for(j=0; j<=length2; j++)
-				{
-					if(File_Contents[currPos][i] == File_Contents[currPos+1][j])
-					{
-						substring[0] = File_Contents[currPos][i];
-						len = 1;
-						x = i+1;
-						y = j+1;
-						while(File_Contents[currPos][x] == File_Contents[currPos+1][y] && x < length1 && y<length2 && len < STRING_SIZE)
-						{
-							substring[len] = File_Contents[currPos][x];
-							len++;
-							x++;
-							y++;
-						}
-						if(len>maxlen)
-						{
-							maxlen = len;
-							strcpy(local_LCS[comp], substring);
-						}
-					}
-				}
-	   		 }
-			comp++;
-		}		//put substring in global array
-		#pragma omp critical
-		{
-			int z = 0;
-			for(currPos = startPos; currPos < endPos; currPos++)
-			{ 
-				strcpy(LCS[currPos], local_LCS[z]);
-				z++;
-			}
+			printf ("\tLines %d-%d:\t", i+1, i+2);
+			for (s = 0; s < result_sizes[i]-1; s++)
+				printf ("%c", LCS[i][s]);
+			printf ("\n");
 		}
 	}
+	else
+	{
+		char *worker_LCS;
+		int LCS_array_sizes[LINES_PER_THREAD];
+		start_pos = rank * LINES_PER_THREAD;
+		end_pos = start_pos + LINES_PER_THREAD;
+		
+		
+		// allocate memory for worker result array
+		// **must be contiguous for mpi**
+		worker_LCS = (char *)malloc (LINES_PER_THREAD * MAX_STRING_SIZE * sizeof (char));
+			
+		// worker variable for worker result size
+		int worker_result_size = 0;
+		
+		// do the thing
+		int o = start_pos;
+		int index = 0;
+		i = 0;
+		while (o < end_pos && o < lines_read-1)
+		{
+			int j, m, n, k, offset;
+			int xlen = strlen (File_Contents[o]);
+			int ylen = strlen (File_Contents[o+1]);
+			char *x = (char *)malloc ((xlen+1) * sizeof (char)); 			// first string
+			char *y = (char *)malloc ((ylen+1) * sizeof (char));			// second string
+		
+			char **b = (char **)malloc ((xlen+1) * sizeof (char *));
+			int **c = (int **)malloc ((xlen+1) * sizeof (int *));
+		
+			// initialize b and c
+			for (j = 0; j <= xlen; j++)
+			{
+				b[j] = (char *)malloc ((ylen+1) * sizeof (char));
+				c[j] = (int *)malloc ((ylen+1) * sizeof (int));
+			}
+		
+			strncpy (x, File_Contents[o], xlen+1);
+			strncpy (y, File_Contents[o+1], ylen+1);
+		
+			lcs (x, y, b, c);
+		
+			// get lcs lengths
+			m = strlen (x);
+			n = strlen (y);		
+			
+			index += (i * MAX_STRING_SIZE) - index;
+			print (x, m, n, b, &index, worker_LCS);
+			LCS_array_sizes[i] = index - (i * MAX_STRING_SIZE);
+			
+			for (k = 0; k < xlen; k++)
+			{
+				free (b[k]);
+				free (c[k]);
+			}
+			
+			free (b);
+			free (c);
+			free (x);
+			free (y);
+			o++, i++, worker_result_size++;
+		}
+		
+		// send stuff to parent process
+		MPI_Send (&worker_result_size, 1, MPI_INT, 0, send_tag*rank+rank, MPI_COMM_WORLD);
+		MPI_Send (LCS_array_sizes, LINES_PER_THREAD, MPI_INT, 0, send_tag*rank+rank+1, MPI_COMM_WORLD);
+		MPI_Send (worker_LCS, LINES_PER_THREAD * MAX_STRING_SIZE, MPI_CHAR, 0, send_tag*rank+rank+2, MPI_COMM_WORLD);
+		
+		free (worker_LCS);
+	}
+	
+	if (root)
+	{
+		end_time = MPI_Wtime ();
+		printf ("\nTime elapsed: %f\n", end_time - start_time);
+	}
+	
+	MPI_Finalize ();
+	return 0;
 }
