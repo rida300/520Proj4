fc4514b6a253b9582d72d58c805d730fe6f01b06
Phillip Carroll <pc6@cougar.cs.ksu.edu>
2019-04-30 00:31:46 -0500

    pthreads works but not how i like

--- proj4/Proj4_pthreads.c
+++ proj4/Proj4_pthreads.c
@@ -4,106 +4,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-int ARTICLE_SIZE =100;
-int STRING_SIZE =15;
+#define ARRAY_SIZE  5
+#define ARTICLE_SIZE 1000
+#define STRING_SIZE 15
+static int NUM_THREADS;
 
-int  NUM_THREADS;
-int ARRAY_SIZE;
 pthread_mutex_t mutexsum;// mutex for LCS
 
-typedef struct {
-  uint32_t virtualMem;
-  uint32_t physicalMem;
-} processMem_t;
-
-char ** LCS;//[ARRAY_SIZE][STRING_SIZE];
-char ** File_Contents;//[ARRAY_SIZE][ARTICLE_SIZE];
+char LCS [ARRAY_SIZE][STRING_SIZE];
+char File_Contents[ARRAY_SIZE][ARTICLE_SIZE];
 void * find_longest_substring(void * id);
 void init_array(FILE *);
 void print_results();
-void GetProcessMemory(processMem_t*);
-int parseLine(char *); 
-void array_init(char *** array, int row, int col);
-
-
-int main(int argc, char ** argv) {
-	  if (argc != 3)
-	{
-		printf ("%s is not a valid inputs\n", argv[0]);
-		return -1;
-	}
-	int myVersion = 2; //base = 1, pthreads = 2, openmp = 3, mpi = 4
-	printf("im in");
-	ARRAY_SIZE = atoi(argv[1]);
-	printf("%d", ARRAY_SIZE);
-	NUM_THREADS = atoi(argv[2]);
-	printf("%d, %d", ARRAY_SIZE, NUM_THREADS);
-	fflush(stdout);
-	struct timeval t1, t2;
-	double elapsedTime;
-	processMem_t procMem;
-	gettimeofday(&t1, NULL);
-	array_init(&LCS, ARRAY_SIZE, STRING_SIZE);
-	//LCS = (char *)malloc(ARRAY_SIZE*STRING_SIZE*sizeof(char));
-	if(LCS == NULL)
-		exit(0);
-	//File_Contents = (char *) malloc(ARRAY_SIZE *ARTICLE_SIZE*sizeof(char));
-	array_init(&File_Contents, ARRAY_SIZE, ARTICLE_SIZE);
-	if(File_Contents == NULL)
-		exit(0);
-
-	printf("i wanna die");
-	fflush(stdout);
-	int i= 0, rc;
-	FILE * fp = fopen ("testFile.txt", "r");
-	pthread_t threads[NUM_THREADS];
-	pthread_attr_t attr;
-	void *status;
-
-
-	/* Initialize and set thread detached attribute */
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-//	exit(0);
-	init_array(fp);
-//	exit(0);
-	for (i = 0; i < NUM_THREADS; i++ ) {
-	      rc = pthread_create(&threads[i], &attr, find_longest_substring, (void *)(intptr_t)i);
-		if (rc) {
-	        printf("ERROR; return code from pthread_create() is %d\n", rc);
-		exit(-1);
-	      }
-	}
-free(File_Contents);
-print("");
-fflush(stdout);
-/* Free attribute and wait for the other threads */
-	pthread_attr_destroy(&attr);
-	for(i=0; i<NUM_THREADS; i++) {
-	     rc = pthread_join(threads[i], &status);
-	     if (rc) {
-		   printf("ERROR; return code from pthread_join() is %d\n", rc);
-		   exit(-1);
-	     }
-//	printf("%d", i);
-//	fflush(stdout);
-	}
-
-	pthread_mutex_destroy(&mutexsum);
-printf("destroyed threads");
-fflush(stdout);
-	print_results(LCS);
-
-	gettimeofday(&t2, NULL);
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; //sec to ms
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
-	GetProcessMemory(&procMem);
-	printf("DATA, %d, %s, %f, %d, %u, %u\n", myVersion, getenv("NSLOTS"), elapsedTime, NUM_THREADS, procMem.virtualMem, procMem.physicalMem);
-	printf("Main: program completed. Exiting.\n");
-	pthread_exit(NULL);
-	return(0);
-}
-
 
 void array_init(char *** array, int row, int col) {
 	int i;
@@ -115,6 +27,12 @@ void array_init(char *** array, int row, int col) {
 	}
 }
 
+typedef struct {
+  uint32_t virtualMem;
+  uint32_t physicalMem;
+} processMem_t;
+
+/* Parses through a pointer or file line-by-line */
 int parseLine(char *line) {
 	// This assumes that a digit will be found and the line ends in " Kb".
 	int i = strlen(line);
@@ -123,14 +41,15 @@ int parseLine(char *line) {
 	line[i - 3] = '\0';
 	i = atoi(p);
 	return i;
-}
+}// end parseLine
 
+/* Gets the Process Memory */
 void GetProcessMemory(processMem_t* processMem) {
 	FILE *file = fopen("/proc/self/status", "r");
 	char line[128];
 
 	while (fgets(line, 128, file) != NULL) {
-		//printf("%s", line);
+		
 		if (strncmp(line, "VmSize:", 7) == 0) {
 			processMem->virtualMem = parseLine(line);
 		}
@@ -140,6 +59,57 @@ void GetProcessMemory(processMem_t* processMem) {
 		}
 	}
 	fclose(file);
+}
+
+int main(int argc, char ** argv) {
+	//ARRAY_SIZE = atoi(argv[1]);
+	NUM_THREADS = atoi(argv[1]);
+	//array_init(&LCS, ARRAY_SIZE-1, STRING_SIZE);
+	//array_init(&File_Contents, ARRAY_SIZE, ARTICLE_SIZE);
+	int myVersion = 2; //base = 1, pthreads = 2, openmp = 3, mpi = 4
+	processMem_t myMem;
+	struct timeval t1, t2;
+	double elapsedTime;
+	gettimeofday(&t1, NULL);
+	int i= 0, rc;
+	FILE * fp = fopen ("testLorem.txt", "r");
+	pthread_t * threads = malloc(sizeof(pthread_t) * NUM_THREADS);
+	pthread_attr_t attr;
+	void *status;
+
+
+	/* Initialize and set thread detached attribute */
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+	init_array(fp);
+	for (i = 0; i < NUM_THREADS-1; i++ ) {
+	      rc = pthread_create(&threads[i], &attr, find_longest_substring, (void *)(intptr_t)i);
+		if (rc) {
+	        printf("ERROR; return code from pthread_create() is %d\n", rc);
+		exit(-1);
+	      }
+	}
+/* Free attribute and wait for the other threads */
+	pthread_attr_destroy(&attr);
+	for(i=0; i<NUM_THREADS-1; i++) {
+	     rc = pthread_join(threads[i], &status);
+	     if (rc) {
+		   printf("ERROR; return code from pthread_join() is %d\n", rc);
+		   exit(-1);
+	     }
+	}
+	pthread_mutex_destroy(&mutexsum);
+
+	print_results(LCS);
+	gettimeofday(&t2, NULL);
+	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; //sec to ms
+	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
+	GetProcessMemory(&myMem);
+	printf("DATA, %d, %s, %f, %d, %d, %d\n", myVersion, getenv("NSLOTS"), elapsedTime, NUM_THREADS, myMem.virtualMem, myMem.physicalMem);
+	printf("Main: program completed. Exiting.\n");
+	pthread_exit(NULL);
+
 }
 
 void init_array(FILE * fp)
@@ -152,14 +122,11 @@ if(fp != NULL)
  char line [ARTICLE_SIZE]; 
       while ( fgets ( line, sizeof line, fp ) != NULL && i < ARRAY_SIZE )
       {
-		strcpy(File_Contents[i], line);
-//		printf("%d\n", i);
-//		fflush(stdout);
-		i++;
+		strcpy(File_Contents[i++], line);
       }
       fclose ( fp );
 }
-
+	
 }
 
 void print_results()
@@ -169,9 +136,7 @@ int j;
   for ( int i = 0; i < ARRAY_SIZE - 1; i++ ) {
   j = i+1;
      printf(" %d & %d - %s\n",i,j, LCS[i]);
-     fflush(stdout);
   }
-
 }
 
 void * find_longest_substring(void * id)//id is 0,1,2,3
@@ -215,9 +180,8 @@ void * find_longest_substring(void * id)//id is 0,1,2,3
 				}
 			}
 			comp++;
-		}
-				//put substring in global array
-		pthread_mutex_lock (&mutexsum);
+		}				//put substring in global array
+			pthread_mutex_lock (&mutexsum);
 			int z = 0;
 			for(currPos = startPos; currPos < endPos; currPos++)
 			{
